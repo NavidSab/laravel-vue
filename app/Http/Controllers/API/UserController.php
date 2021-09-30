05a49d56679ac841123bbdf199de9cf3c5f0f296
navid <navidsky10@gmail.com>
2021-09-30 13:24:30 +0330

    fix

---
+++ app/Http/Controllers/API/UserController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Repositories\UserRepo;
+class UserController extends Controller
+{
+
+
+
+    protected $userRepo;
+
+    /**
+     * Assign User Repo
+     * @param instance $userRepo
+     * @return Object
+     */
+    public function __construct(UserRepo $userRepo){
+        $this->userRepo=$userRepo;
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+//
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+       $user= $this->userRepo->store($request);
+       return $user;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/js/components/User.vue
@@ -0,0 +1,155 @@
+<template>
+    <div class="container">
+        <div class="row mt-4">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">User Table</h3>
+                        <div class="card-tools">
+                            <button class="btn btn-success" data-toggle="modal" data-target="#addNewCenter">
+                                Add New
+                                <i class="fas  fa-user-plus fa-fw"></i>
+                            </button>
+                        </div>
+                    </div>
+                    <!-- /.card-header -->
+                    <div class="card-body table-responsive p-0">
+                        <table class="table table-hover text-nowrap">
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Name</th>
+                                    <th>Email</th>
+                                    <th>Type</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>183</td>
+                                    <td>John Doe</td>
+                                    <td>11-7-2014</td>
+                                    <td>
+                                        <span class="tag tag-success">Approved</span>
+                                    </td>
+                                    <td>
+                                        <a href="#">
+                                            <i class="fa fa-edit blue"></i>
+                                        </a>
+                                        /
+                                        <a href="#">
+                                            <i class="fa fa-trash red"></i>
+                                        </a>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <!-- /.card-body -->
+                </div>
+                <!-- /.card -->
+            </div>
+        </div>
+        <!-- Modal -->
+        <div
+            class="modal fade"
+            id="addNewCenter"
+            tabindex="-1"
+            role="dialog"
+            aria-labelledby="addNewCenterTitle"
+            aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="addNewLongTitle">Add New</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <form @submit.prevent="createUser">
+                            <div class="mb-3">
+                                <input
+                                    id="name"
+                                    v-model="form.name"
+                                    type="text"
+                                    name="name"
+                                    class="form-control"
+                                    placeholder="Name">
+                                    <HasError :form="form" field="name"/>
+                                </div>
+                                <div class="mb-3">
+                                    <input
+                                        id="email"
+                                        v-model="form.email"
+                                        type="text"
+                                        name="email"
+                                        class="form-control"
+                                        placeholder="Email Address">
+                                        <HasError :form="form" field="email"/>
+                                    </div>
+                                    <div class="mb-3">
+                                        <textarea
+                                            name="bio"
+                                            v-model="form.bio"
+                                            class="form-control"
+                                            id="bio"
+                                            placeholder="Bio"></textarea>
+                                        <HasError :form="form" field="bio"/>
+                                    </div>
+                                    <div class="mb-3">
+                                        <select id="type" v-model="form.type" class="form-control">
+                                            <option value="">Select One Type</option>
+                                            <option value="admin">Admin</option>
+                                            <option value="user">User</option>
+                                            <option value="author">Author</option>
+                                        </select>
+                                        <HasError :form="form" field="type"/>
+                                    </div>
+                                    <div class="mb-3">
+                                        <input
+                                            id="password"
+                                            v-model="form.password"
+                                            type="password"
+                                            name="password"
+                                            class="form-control"
+                                            placeholder="Password">
+                                            <HasError :form="form" field="password"/>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                                            <button type="submit" class="btn btn-primary">Create</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </template>
+            <script>
+                export default {
+                    data() {
+                        return {
+                            form: new Form({
+                                name: '',
+                                email: '',
+                                password: '',
+                                type: '',
+                                bio: '',
+                                photo: ''
+                            })
+                        }
+                    },
+                    methods: {
+                        createUser() {
+                            this
+                                .form
+                                .post('api/user');
+                        }
+                    },
+                    mounted() {
+                        console.log('Component mounted.')
+                    }
+                }
+            </script>
